7fe5af3f2c3037d6ac2db521ba2ca0dd50eec205
carolinevianab <carolinevb123@gmail.com>
2021-03-02 15:47:16 -0300

    tela qualidade do ar atualizada

--- Nano5/Nano5/ViewControllers/DetalheArViewController.swift
+++ Nano5/Nano5/ViewControllers/DetalheArViewController.swift
@@ -20,6 +20,8 @@ class DetalheArViewController: UIViewController {
     @IBOutlet weak var lblPM2_5: UILabel!
     @IBOutlet weak var lblPM10: UILabel!
     @IBOutlet weak var lblNH3: UILabel!
+    @IBOutlet weak var lblDicaAr: UILabel!
+    @IBOutlet weak var lblQualidade: UILabel!
     
     // MARK: viewDidLoad
     override func viewDidLoad() {
@@ -32,12 +34,44 @@ class DetalheArViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         lblCO.text = "CO: \(String(qualAr?.list?.first??.components?.co ?? 1234)) μg/m³"
         lblNO.text = "NO: \(String(qualAr?.list?.first??.components?.no ?? 1234)) μg/m³"
-        lblNO2.text = "NO2: \(String(qualAr?.list?.first??.components?.no2 ?? 1234)) μg/m³"
-        lblO3.text = "O3: \(String(qualAr?.list?.first??.components?.o3 ?? 1234)) μg/m³"
-        lblSO2.text = "SO2: \(String(qualAr?.list?.first??.components?.so2 ?? 1234)) μg/m³"
-        lblPM2_5.text = "PM2_5: \(String(qualAr?.list?.first??.components?.pm2_5 ?? 1234)) μg/m³"
-        lblPM10.text = "PM10: \(String(qualAr?.list?.first??.components?.pm10 ?? 1234)) μg/m³"
-        lblNH3.text = "NH3: \(String(qualAr?.list?.first??.components?.nh3 ?? 1234)) μg/m³"
+        lblNO2.text = "NO₂: \(String(qualAr?.list?.first??.components?.no2 ?? 1234)) μg/m³"
+        lblO3.text = "O₃: \(String(qualAr?.list?.first??.components?.o3 ?? 1234)) μg/m³"
+        lblSO2.text = "SO₂: \(String(qualAr?.list?.first??.components?.so2 ?? 1234)) μg/m³"
+        lblPM2_5.text = "PM₂₅: \(String(qualAr?.list?.first??.components?.pm2_5 ?? 1234)) μg/m³"
+        lblPM10.text = "PM₁₀: \(String(qualAr?.list?.first??.components?.pm10 ?? 1234)) μg/m³"
+        lblNH3.text = "NH₃: \(String(qualAr?.list?.first??.components?.nh3 ?? 1234)) μg/m³"
+        
+        switch qualAr?.list?.first??.main?.aqi {
+        case 1:
+            lblQualidade.text = "Qualidade do ar: Ótima"
+            lblDicaAr.text = "Tudo certo :)"
+            break
+        case 2:
+            lblQualidade.text = "Qualidade do ar: Boa"
+            lblDicaAr.text = "Tudo certo :)"
+            break
+        case 3:
+            lblQualidade.text = "Qualidade do ar: Moderada"
+            lblDicaAr.text = "Se você tem problemas respiratórios, pode ter alguns sintomas mais leves, como tosse seca."
+            break
+        case 4:
+            lblQualidade.text = "Qualidade do ar: Ruim"
+            lblDicaAr.text = "Você pode ter tosse seca e ardor nos olhos, nariz e garganta."
+            break
+        case 5:
+            lblQualidade.text = "Qualidade do ar: Muito ruim"
+            lblDicaAr.text = "Você pode ter sintomas de problemas respiratórios."
+            break
+        default:
+            print("foi pro default e deu errado")
+            break
+        }
+        print(view.frame.size.height)
+        if view.frame.size.height < 630 {
+            
+        }
+        
+        
     }
     
     // MARK: IBAction fechar
